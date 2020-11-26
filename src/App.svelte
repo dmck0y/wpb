c2bb0efc299ce0b77018451fdecaecd5881636f7
dmck0y <dmckoy85@gmail.com>
2020-11-25 22:17:59 -0500

    update layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,42 @@
 <script>
-	export let name;
 	import ArtboardBtn from './ArtboardBtn.svelte';
 </script>
 
 <main>
-	<img href="favicon.png" />
+	<!-- <img href="favicon.png" /> -->
 	<h1 class="logo-type">wastepaperbasket</h1>
-	<div class="artboard">
-		<ArtboardBtn>prev</ArtboardBtn>
+	<div class="artboard full">
+		<nav class="image-nav full">
+			<ArtboardBtn>prev</ArtboardBtn>
+			<ArtboardBtn>next</ArtboardBtn>
+		</nav>
 	</div>
-	<a>gallery</a>
 </main>
 
 <style>
 	main {
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
 	.logo-type {
+		position: absolute;
 		font-size: 1.5rem;
 		font-weight: 500;
-		transform: translate(35px, 20px) rotate(90deg);
+		transform: translate(75px, 35px) rotate(90deg);
 		transform-origin: left center; 
+		z-index: 20;
 	}
 
 	.artboard {
-		margin: 4rem;
+		margin: 6rem 3rem 3rem;
 		background: rgba(197, 197, 197, .25);
-		position: absolute;
-		top: 0;
-		bottom: 0;
-		left: 0;
-		right: 0;
 	}
 
-  a {
-		position: absolute;
-		bottom: 0;
-		display: block;
-		margin: 1rem;
+	.image-nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		z-index: 10;
 	}
 
 	@media (min-width: 640px) {
--- src/ArtboardBtn.svelte
+++ src/ArtboardBtn.svelte
@@ -1,14 +1,31 @@
 <script>
 </script>
 
-<div class="button">
-  <slot></slot>
-</div>
+<button class="button-wrapper">
+  <div class="button">
+    <slot></slot>
+  </div>
+</button>
 
 <style>
+
+  .button-wrapper{
+    padding: 0;
+    border: none;
+    cursor: pointer;
+  }
+
+  .button-wrapper:focus,
+  .button-wrapper:hover {
+    
+  }
   .button {
-    width: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 60px;
     height: 40px;
+    padding: 0 0 2px;
     background: white;
   }
 </style>
